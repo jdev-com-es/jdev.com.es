5552da94f12948a0f5305e8cac0e3d03a8a48ddb
jancraft888 <jancraft888@gmail.com>
2022-05-11 18:44:13 +0200

    added articles, rss feed, landing is now black

--- build.js
+++ build.js
@@ -20,15 +20,16 @@ await fs.copy(packjson.build.sourceDir, packjson.build.outputDir, { filter: (src
         const spl = content.split('\n')
         const defhead = spl[0]
         const tplid = defhead.substring(defhead.indexOf('TEMPLATE:')+9).split(' ')[0]
-        let finalFile = templates[tplid].replace('<!-- TEMPLATE:CONTENT -->', content)
+        let finalFile = templates[tplid].replaceAll('<!-- TEMPLATE:CONTENT -->', content)
         let head = 1
         while (spl[head].indexOf('TEMPLATE:OVERWRITE') != -1) {
             const toks = spl[head].substring(spl[head].indexOf('TEMPLATE:OVERWRITE ')+19).split(' ')
             const repl = toks.slice(1, toks.length - 1).join(' ')
-            finalFile = finalFile.replace(`<!-- TEMPLATE:DATA ${toks[0]} -->`, repl)
+            finalFile = finalFile.replaceAll(`<!-- TEMPLATE:DATA ${toks[0]} -->`, repl)
             head++
         }
-        fs.writeFileSync(dst.replace('.tpl.html', '.html'), finalFile)
+        finalFile = finalFile.replaceAll('$$relpath$$', dst.substring(packjson.build.outputDir.length + 1).replace('.tpl.html', ''))
+        fs.writeFileSync(dst.replaceAll('.tpl.html', '.html'), finalFile)
         return false
     }
     return true
